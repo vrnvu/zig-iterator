88b9934c53996c55b8a5baf908bfc763fc606561
Arnau Diaz <arnau.diaz@adevinta.com>
2022-05-13 22:42:54 +0200

    wip

--- src/main.zig
+++ src/main.zig
@@ -137,3 +137,99 @@ fn factorial(n: u32) !u32 {
 test "factorial fn" {
     try testing.expectEqual(factorial(10), 1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9);
 }
+
+pub fn Stringer() type {
+    return struct {
+        const Self = @This();
+
+        iterator: Iterator(u8),
+        string: []const u8,
+        p: usize,
+
+        pub fn init(string: []const u8) !Self {
+            return Self{
+                .iterator = Iterator(u8){
+                    .nextFn = next,
+                },
+                .string = string,
+                .p = 0,
+            };
+        }
+
+        pub fn next(iterator: *Iterator(u8)) ?u8 {
+            const self = @fieldParentPtr(Self, "iterator", iterator);
+            if (self.p >= self.string.len) {
+                return null;
+            }
+            const current = self.string[self.p];
+            self.p += 1;
+            return current;
+        }
+    };
+}
+
+test "stringer" {
+    const string = "hello";
+    var range = try Stringer().init(string[0..]);
+    var iter = &range.iterator;
+    var i: usize = 0;
+    while (iter.next()) |c| {
+        try testing.expectEqual(string[i], c);
+        i += 1;
+    }
+    try testing.expectEqual(i, string.len);
+    try testing.expectEqual(iter.next(), null);
+}
+
+pub fn SinglyLinkedList(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        iterator: Iterator(T),
+        list: std.SinglyLinkedList(T),
+
+        pub fn init(list: std.SinglyLinkedList(T)) !Self {
+            return Self{
+                .iterator = Iterator(T){
+                    .nextFn = next,
+                },
+                .list = list,
+            };
+        }
+
+        pub fn next(iterator: *Iterator(T)) ?T {
+            const self = @fieldParentPtr(Self, "iterator", iterator);
+            if (self.list.popFirst()) |current| {
+                return current.data;
+            } else {
+                return null;
+            }
+        }
+    };
+}
+
+test "singly linked list" {
+    const L = std.SinglyLinkedList(u32);
+    var list = L{};
+
+    try testing.expect(list.len() == 0);
+
+    var one = L.Node{ .data = 1 };
+    var two = L.Node{ .data = 2 };
+    var three = L.Node{ .data = 3 };
+
+    list.prepend(&three);
+    list.prepend(&two);
+    list.prepend(&one);
+
+    try testing.expect(list.len() == 3);
+
+    var iterator = try SinglyLinkedList(u32).init(list);
+    var iter = &iterator.iterator;
+    var c: u32 = 1;
+
+    while (iter.next()) |got| {
+        try testing.expectEqual(c, got);
+        c += 1;
+    }
+}
